b6947459cefe264378e7bfe0e997952d1727ca82
Mccranky83 <ericcai20021118@gmail.com>
2024-01-17 16:27:32 +0800

    feat(server): Implement Express production server; chore(scripts): Add npm start script to launch server

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.join(path.dirname(__filename), "/");
+const publicPath = path.join(__dirname, "../dist/");
+const portNumber = process.env.PORT || 5373;
+console.log(__dirname);
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(`${publicPath}`, "index.html"));
+});
+
+app.listen(portNumber, () => {
+  console.log(`App started on port http://localhost:${portNumber}...`);
+});
